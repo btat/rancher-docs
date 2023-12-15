05104ef605e6faac1f83085a67cf20f139de734c
Billy Tat <btat@suse.com>
2023-12-15 13:35:57 -0800

    test123

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@
 const tailwindPlugin = require('./plugins/tailwind-plugin.cjs');
 
 module.exports = {
-  title: 'Rancher',
+  title: 'RancherABC',
   tagline: '',
   url: 'https://ranchermanager.docs.rancher.com/',
   baseUrl: '/',
